8b2f04a33c8e2d72cbfb33e139482a3ab9aa1f4f
Margaret Yeh <margyeh7@gmail.com>
2022-10-30 12:09:21 -0700

    remade package to conform to name, fixed POCv1 to conform to Decrypter protocol

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "crack_station",
+    name: "CrackStation",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "crack_station",
-            targets: ["crack_station"]),
+            name: "CrackStation",
+            targets: ["CrackStation"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "crack_station",
-            dependencies: []),
+            name: "CrackStation",
+            dependencies: [],
+            resources: [ 
+                .process("data.json")
+            ]
+            ),
         .testTarget(
-            name: "crack_stationTests",
-            dependencies: ["crack_station"]),
+            name: "CrackStationTests",
+            dependencies: ["CrackStation"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # crack_station
 
-A description of this package.
+A description of your library.  What does your library do?  What’s it for?  Why might someone want to use it or care?
+# Description
+Takes an input SHA1 hash and outputs the corresponding character.
+POCv1 (current): Allows input for single character hashes (a-z, A-Z, 0-9).
+POCv2: Allows input for 1,2 character hashes
+POCv3: Allows input for 1,2,3 character hashes for both SHA1 and SHA256
+# Use
+A description of how to use it.  What’s the public API, etc?
+
+# Version
+1.0.0
+
+# Author
+Margaret Yeh
+
