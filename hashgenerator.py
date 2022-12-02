dde4b35db145851733ce3ffd1162ec6d47614b08
Margaret Yeh <margyeh7@gmail.com>
2022-12-01 21:47:10 -0800

    new json to include ? and ! characters

--- hashgenerator.py
+++ hashgenerator.py
@@ -4,42 +4,83 @@ import string
 import sys
 
 sys.stdout.write("{")
-for letter in (string.ascii_letters + string.digits):
+for letter in (string.ascii_letters + string.digits +'?' + '!'):
     str = letter
     result = hashlib.sha1(str.encode())
     sys.stdout.write("\n\"" + result.hexdigest() + "\": \"" + letter + "\",")
 
-for letter1 in (string.ascii_letters + string.digits):
-    for letter2 in (string.ascii_letters + string.digits):
+for letter1 in (string.ascii_letters + string.digits +'?' + '!'):
+    for letter2 in (string.ascii_letters + string.digits +'?' + '!'):
         str = letter1+letter2
         result = hashlib.sha1(str.encode())
         sys.stdout.write("\n\"" + result.hexdigest() + "\": \"" + str + "\",")
 
-for letter1 in (string.ascii_letters + string.digits):
-    for letter2 in (string.ascii_letters + string.digits):
-        for letter3 in (string.ascii_letters + string.digits):
+for letter1 in (string.ascii_letters + string.digits +'?' + '!'):
+    for letter2 in (string.ascii_letters + string.digits +'?' + '!'):
+        for letter3 in (string.ascii_letters + string.digits +'?' + '!'):
             str = letter1+letter2+letter3
             result = hashlib.sha1(str.encode())
             sys.stdout.write("\n\"" + result.hexdigest() + "\": \"" + str + "\",")
 
-for letter in (string.ascii_letters + string.digits):
+for letter in (string.ascii_letters + string.digits +'?' + '!'):
     str = letter
     result = hashlib.sha256(str.encode())
     sys.stdout.write("\n\"" + result.hexdigest() + "\": \"" + letter + "\",")
 
-for letter1 in (string.ascii_letters + string.digits):
-    for letter2 in (string.ascii_letters + string.digits):
+for letter1 in (string.ascii_letters + string.digits +'?' + '!'):
+    for letter2 in (string.ascii_letters + string.digits +'?' + '!'):
         str = letter1+letter2
         result = hashlib.sha256(str.encode())
         sys.stdout.write("\n\"" + result.hexdigest() + "\": \"" + str + "\",")
 
-for letter1 in (string.ascii_letters + string.digits):
-    for letter2 in (string.ascii_letters + string.digits):
-        for letter3 in (string.ascii_letters + string.digits):
+for letter1 in (string.ascii_letters + string.digits +'?' + '!'):
+    for letter2 in (string.ascii_letters + string.digits +'?' + '!'):
+        for letter3 in (string.ascii_letters + string.digits +'?' + '!'):
             str = letter1+letter2+letter3
             result = hashlib.sha256(str.encode())
             sys.stdout.write("\n\"" + result.hexdigest() + "\": \"" + str + "\"")
-            if str != "999":
+            if str != "!!!":
                 sys.stdout.write(",")
 
 sys.stdout.write("\n}")
+
+#---------------------------------------
+#.CSV
+#---------------------------------------
+# print("shaHash,password")
+
+# for letter in (string.ascii_letters + string.digits):
+#     str = letter
+#     result = hashlib.sha1(str.encode())
+#     print(result.hexdigest() + "," + str)
+
+# for letter1 in (string.ascii_letters + string.digits):
+#     for letter2 in (string.ascii_letters + string.digits):
+#         str = letter1+letter2
+#         result = hashlib.sha1(str.encode())
+#         print(result.hexdigest() + "," + str)
+
+# for letter1 in (string.ascii_letters + string.digits):
+#     for letter2 in (string.ascii_letters + string.digits):
+#         for letter3 in (string.ascii_letters + string.digits):
+#             str = letter1+letter2+letter3
+#             result = hashlib.sha1(str.encode())
+#             print(result.hexdigest() + "," + str)
+
+# for letter in (string.ascii_letters + string.digits):
+#     str = letter
+#     result = hashlib.sha256(str.encode())
+#     print(result.hexdigest() + "," + str)
+
+# for letter1 in (string.ascii_letters + string.digits):
+#     for letter2 in (string.ascii_letters + string.digits):
+#         str = letter1+letter2
+#         result = hashlib.sha256(str.encode())
+#         print(result.hexdigest() + "," + str)
+
+# for letter1 in (string.ascii_letters + string.digits):
+#     for letter2 in (string.ascii_letters + string.digits):
+#         for letter3 in (string.ascii_letters + string.digits):
+#             str = letter1+letter2+letter3
+#             result = hashlib.sha256(str.encode())
+#             print(result.hexdigest() + "," + str)
